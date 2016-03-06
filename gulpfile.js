12bc6ce8060a5a4d7e1e21b49ceb0b2dc0bf276e
Dzulqarnain Nasir <dzul1983@gmail.com>
2016-03-06 19:01:32 +0200

    Updated tooling.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp');
-var uglifyjs = require('gulp-uglifyjs');
+var uglify = require('gulp-uglify');
 var header = require('gulp-header');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
 var pkg = require('./package.json');
 
-var banner = '/*! <%= pkg.name %> <%= pkg.version %> | (c) 2015 <%= pkg.author %> | <%= pkg.license %> */\n';
+var year = new Date().getFullYear();
+var banner = '/*! <%= pkg.name %> <%= pkg.version %> | (c) ' + year + ' <%= pkg.author %> | <%= pkg.license %> */\n';
 
 gulp.task('js', function() {
     gulp.src('src/jquery.cascadingdropdown.js')
-        .pipe(gulp.dest('dist/'))
-        .pipe(uglifyjs('jquery.cascadingdropdown.min.js', { outSourceMap: true }))
         .pipe(header(banner, { pkg: pkg }))
+        .pipe(gulp.dest('dist/'))
+        .pipe(rename('jquery.cascadingdropdown.min.js'))
+        .pipe(sourcemaps.init())
+        .pipe(uglify({ preserveComments: 'license' }))        
+        .pipe(sourcemaps.write('./'))
         .pipe(gulp.dest('dist/'));
 });
 
