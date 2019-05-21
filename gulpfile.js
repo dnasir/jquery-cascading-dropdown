365480aa63c109501ae826690d9be364e42059b9
Dzulqarnain Nasir <dzul1983@gmail.com>
2019-05-21 13:11:06 +0300

    Updated dependencies and fixed gulpfile
    Should fix #79

--- gulpfile.js
+++ gulpfile.js
@@ -3,20 +3,45 @@ var uglify = require('gulp-uglify');
 var header = require('gulp-header');
 var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
+var connect = require('gulp-connect');
 var pkg = require('./package.json');
 
 var year = new Date().getFullYear();
-var banner = '/*! <%= pkg.name %> <%= pkg.version %> | (c) ' + year + ' <%= pkg.author %> | <%= pkg.license %> */\n';
+var banner =
+  '/*! <%= pkg.name %> <%= pkg.version %> | (c) ' +
+  year +
+  ' <%= pkg.author %> | <%= pkg.license %> */\n';
 
-gulp.task('js', function () {
-    gulp.src('src/jquery.cascadingdropdown.js')
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('dist/'))
-        .pipe(rename('jquery.cascadingdropdown.min.js'))
-        .pipe(sourcemaps.init())
-        .pipe(uglify({ preserveComments: 'license' }))
-        .pipe(sourcemaps.write('./'))
-        .pipe(gulp.dest('dist/'));
+gulp.task('js', function() {
+  return gulp
+    .src('src/jquery.cascadingdropdown.js')
+    .pipe(header(banner, { pkg: pkg }))
+    .pipe(gulp.dest('dist/'))
+    .pipe(rename('jquery.cascadingdropdown.min.js'))
+    .pipe(sourcemaps.init())
+    .pipe(
+      uglify({
+        output: {
+          comments: /^!/
+        }
+      })
+    )
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('dist/'))
+    .pipe(connect.reload());
 });
 
-gulp.task('default', ['js']);
+gulp.task('webserver', function(done) {
+  connect.server({
+    livereload: true
+  });
+  done();
+});
+
+gulp.task('watch', function(done) {
+  gulp.watch('src/jquery.cascadingdropdown.js', gulp.series(['js']));
+  done();
+});
+
+gulp.task('default', gulp.series(['js']));
+gulp.task('dev', gulp.parallel(['webserver', 'watch']));
